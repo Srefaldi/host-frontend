6e05a4ce0073b4f5eb65101cc81acd605d802785
Srefaldi <2110131110004@mhs.ulm.ac.id>
2025-05-29 12:03:18 +0800

    update lagi

--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,8 @@ axios.defaults.withCredentials = true;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-    <App />
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
